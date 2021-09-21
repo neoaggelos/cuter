a9b5fe79d1dcea3a04e51c8538d22c08e2dac86d
Aggelos Kolaitis <neoaggelos@gmail.com>
2021-09-22 00:03:51 +0300

    Extend map evaluation tests

--- test/ftest/src/map.erl
+++ test/ftest/src/map.erl
@@ -1,5 +1,5 @@
 -module(map).
--export([simple/1, simple_squared/1]).
+-export([simple/1, simple_squared/1, simple_extend/1, extend_empty/1]).
 
 -spec simple(integer()) -> #{value => float()}.
 simple(X) ->
@@ -8,3 +8,13 @@ simple(X) ->
 -spec simple_squared(integer()) -> #{value => float()}.
 simple_squared(X) ->
     #{ value => 10 / (X*X - 4) }.
+
+-spec simple_extend(integer()) -> #{value => float()}.
+simple_extend(X) ->
+    A = #{ a => X },
+    A#{ value => 10 / (X*X - 4) }.
+
+-spec extend_empty(integer()) -> #{value => float()}.
+extend_empty(X) ->
+    A = #{},
+    A#{ value => 10 / (X*X - 4) }.
